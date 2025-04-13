c84bf005b98f28f830b40c536299ca6ad19350f0
Vladimir <k.vovan.n@gmail.com>
2025-04-13 09:02:06 +0300

    Fixed class InputClass

--- hws2-master/src/s2-homeworks/hw03/Greeting.tsx
+++ hws2-master/src/s2-homeworks/hw03/Greeting.tsx
@@ -25,7 +25,9 @@ const Greeting: React.FC<GreetingPropsType> = (
     lastUserName,
   } // деструктуризация пропсов
 ) => {
-  const inputClass = s.errorInput // need to fix with (?:)
+  // const inputClass = s.errorInput // need to fix with (?:)
+  const inputClass = error ? s.errorInput : ''
+
 
   return (
     <div id={'hw3-form'} className={s.greetingForm}>
