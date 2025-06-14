1c8606a37c31dcdf3ff878b6f2c0f3c5dada6edd
Vladimir <k.vovan.n@gmail.com>
2025-06-14 06:55:18 +0300

    start WH9

--- hws2-master/src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ hws2-master/src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,15 +8,15 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
     switch (action.type) {
         case 'sort': { // by name
 let sorting;
-if (action.payload=="up") {
+if (action.payload==="up") {
   sorting = state.sort((a, b)=> (a.name > b.name )? 1 : -1)}
-if (action.payload=="down") {
+if (action.payload==="down") {
   sorting = state.sort((a, b)=> (a.name < b.name )? 1 : -1)}
             return sorting // need to fix
         }
         case 'check': {
           const teenagerNumb= Number(action.payload) 
-            return state.filter((elem)=>elem.age > teenagerNumb ) // need to fix
+            return state.sort((a, b)=>a.age-b.age ? -1 : 1).filter((elem)=>elem.age > teenagerNumb ) // need to fix
         }
         default:
             return state
