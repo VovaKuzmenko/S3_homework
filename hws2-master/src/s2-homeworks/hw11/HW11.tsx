f39e016b4f2f30e92554c661896e934ff164cff6
Vladimir <k.vovan.n@gmail.com>
2025-10-08 20:57:23 +0300

    Added comments on the solution to the problem in the comments

--- hws2-master/src/s2-homeworks/hw11/HW11.tsx
+++ hws2-master/src/s2-homeworks/hw11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { MouseEventHandler, useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
@@ -11,41 +11,60 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 * */
 
 function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+  // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+  const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+  const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    }
+  const change = (event: React.MouseEventHandler<HTMLSpanElement>, value: number) => {
+    // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
 
-    return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+    /************************************************** */
+    // Тут изменение значения записываются в стейт и потом перепысываются.
+    setValue1(20)
+    setValue2(40)
+  }
 
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
+  return (
+    <div id={'hw11'}>
+      <div className={s2.hwTitle}>Homework #11</div>
 
-                        />
-                    </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
+      <div className={s2.hw}>
+        <div className={s.container}>
+          <div className={s.wrapper}>
+            <span id={'hw11-value'} className={s.number}>{value1}</span>
+            <SuperRange
+              id={'hw11-single-slider'}
+            // сделать так чтоб value1 изменялось // пишет студент
+            /************************************************** */
+            // 3. Задается значение 2-го ползунка
+            // value={[10]}
+            /************************************************** */
+            />
+          </div>
+          <div className={s.wrapper}>
+            <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+            <SuperRange
+              //! value={[1, 10]} - элемнт отображения шариков в процентах от ширины экрана; отображая в width на картинке
+              //! соответственно нам рассчитывать необходимо от ширины на экране, а не от общей
+              //! событие мышкой задает величину и передает ее в массив ползунка
 
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
-                    </div>
-                </div>
-            </div>
+              // todo Алглритм:
+              // * 1. Передать "величину" событие мышкой (верхний ползунок) в useState
+              // * 2. Окрасить следующую за ней область в зеленый цвет (не обязательно)
+              // * 3. Привязать ползунок 2 к первому через useState
+              // * 4. сделать 2-й шарик (ползунок 2) и  к передавать его значение useState
+              id={'hw11-double-slider'}
+            // сделать так чтоб value1/2 изменялось // пишет студент
+            /************************************************** */
+            // value={[10, 15]}
+            /************************************************** */
+            />
+            <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default HW11
--- hws2-master/src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ hws2-master/src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,52 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React, { useRef } from 'react'
+import { Slider, SliderProps } from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+  console.log(props)
+  return (
+    <Slider
+      sx={{ // стили для слайдера // пишет студент
+        color: '#1976d2',
+        height: 6,
+        '& .MuiSlider-thumb': {
+          height: 22,
+          width: 22,
+          backgroundColor: '#fff',
+          border: '2px solid #42e732',
+          position: 'relative',
+          '&::after': {
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 10,
+            height: 10,
+            borderRadius: '50%',
+            background: '#42e732',
+          },
+        },
+        '& .MuiSlider-rail': {
+          opacity: 1,
+          backgroundColor: '#b0b0b0',
+          borderRadius: 3,
+          // width: '200px', // длинна всего трека
+          width: '20%', // длинна всего трека
+        },
+        '& .MuiSlider-track': {
+          backgroundColor: '#42e732',
+          borderColor: '#42e732',
+          borderRadius: 3,
+          // maxWidth: '200px', // длинна зеленого распространения
+          maxWidth: '20%', // длинна зеленого распространения
+        },
+
+      }}
+
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  )
 }
 
 export default SuperRange
